b2d430e8da000dc26183cc16b6f231fde6a9b3bb
krajca_michael <michael.krajca007@seznam.cz>
2024-05-31 11:29:47 +0200

    pátek

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -4,13 +4,16 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\Okres;
+use App\Models\Obec;
 
 class Main extends BaseController
 {
     var $okres;
     var $data;
+    var $obec;
     public function __construct(){
         $this->okres= new Okres();
+        $this->obec= new Obec();
         $okresy = $this->okres->where("kraj", 141)->findall();
         $this->data["okresy"] = $okresy;
     }
@@ -20,7 +23,17 @@ class Main extends BaseController
     }
     public function getOkres($id)
     {
-        $data["okres"] = $this->okres->join('obec', 'obec.okres = "okres.kod',"inner")->find($id);
+        $perPage = $this->request->getVar("per_page") ?? 20;
+        $this->data["obec"] = $this->obec
+        ->select("obec.nazev, count(*) as pocet")
+        ->where("okres", $id)->join('cast_obce', 'obec.kod = cast_obce.obec',"inner")
+        ->join('ulice', 'ulice.cast_obce = cast_obce.kod',"inner")
+        ->join('adresni_misto', 'adresni_misto.ulice = ulice.kod',"inner")
+        ->groupBy("obec.kod")
+        ->orderBy("pocet", "desc")
+        ->paginate(20);
+        $this->data["pager"] = $this->obec->pager;
+        $this->data["per_page"] = $perPage;
 
         echo view("nejakyOkres", $this->data);
     }
--- app/Views/nejakyOkres.php
+++ app/Views/nejakyOkres.php
@@ -1,6 +1,64 @@
 <?php
 $this->extend("layout/layout");
-$this->section("content"); ?>
+$this->section("content"); 
+$table = new \CodeIgniter\View\Table();
+
+$table->setHeading(['Pořadí', 'Název', 'Počet']);
+$pageNum = $pager->getCurrentPage();
+$offset = ($pageNum - 1) * $pager->getPerPage();
+foreach ($obec as $key => $row){
     
-<?php 
+    $table->addRow($offset +$key + 1, $row->nazev, $row->pocet);
+}
+
+$template = [
+    'table_open' => '<table class="table table-light">',
+
+    'thead_open'  => '<thead>',
+    'thead_close' => '</thead>',
+
+    'heading_row_start'  => '<tr>',
+    'heading_row_end'    => '</tr>',
+    'heading_cell_start' => '<th>',
+    'heading_cell_end'   => '</th>',
+
+    'tfoot_open'  => '<tfoot>',
+    'tfoot_close' => '</tfoot>',
+
+    'footing_row_start'  => '<tr>',
+    'footing_row_end'    => '</tr>',
+    'footing_cell_start' => '<td>',
+    'footing_cell_end'   => '</td>',
+
+    'tbody_open'  => '<tbody>',
+    'tbody_close' => '</tbody>',
+
+    'row_start'  => '<tr>',
+    'row_end'    => '</tr>',
+    'cell_start' => '<td>',
+    'cell_end'   => '</td>',
+
+    'row_alt_start'  => '<tr>',
+    'row_alt_end'    => '</tr>',
+    'cell_alt_start' => '<td>',
+    'cell_alt_end'   => '</td>',
+
+    'table_close' => '</table>',
+];
+
+$table->setTemplate($template);
+
+echo $table->generate();
+echo $pager->links();
+?>
+<form method="get" action="">
+    <label for="per_page"> Počet položek na stránku:</label>
+    <select id="per_page" name="per_page" onchange="this.form.submit()">
+      <option value="10" <?= ($per_page == 10) ? "selected" : "" ?>>10</option>
+      <option value="20" <?= ($per_page == 20) ? "selected" : "" ?>>20</option>
+      <option value="50" <?= ($per_page == 50) ? "selected" : "" ?>>50</option>
+      <option value="100" <?= ($per_page == 100) ? "selected" : "" ?>>100</option>
+  </select>
+</form>
+<?php
   $this->endSection();?>
